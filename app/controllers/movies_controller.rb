833be0be3df7648238be746f8239e091a380a2a1
Titus Mumo <titusmumo@gmail.com>
2022-11-29 18:13:42 +0300

    Done

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -5,4 +5,25 @@ class MoviesController < ApplicationController
     render json: movies
   end
 
+  def create
+    movie = Movie.create(toy_params)
+    render json: movie, status: :created
+  end
+
+  def update
+    movie = Movie.find_by(id: params[:id])
+    movie.update(toy_params)
+  end
+
+  def destroy
+    movie = Movie.find_by(id: params[:id])
+    movie.destroy
+    head :no_content
+  end
+
+  private
+  
+  def toy_params
+    params.permit(:title, :year, :length, :director, description, :poster_url, :category)
+  end
 end
